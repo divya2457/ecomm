b73ca4462813fc02c1b089e4856071c7b9f939c1
divya2457 <divyaramesh2457@gmail.com>
2025-07-15 13:16:56 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,33 @@
+const studentdetails=function(name,course,duration,fees)
+{
+    console.log("the student details are:",name,course,duration,fees);
+}
+studentdetails("divi","bsc","3months",20000)//
+
+
+
+
+const studentdetails2=function(){
+    let name1="rithu"
+    let course="Mbbs"
+    let duration="5 years"
+    console.log("The studrnt details are:",( name1+course+duration));
+}
+studentdetails2();
+
+const fruits=function()
+{
+    let apple=400
+        orange=200
+        banana=50
+    console.log("total amount is",(apple+orange+banana));
+}
+fruits();//
+
+let student =
+{
+    studentname:"bala",
+    age:22
+}
+console.log(student);   
+
